0c93e12f5f534bb64ba3f0e55c9bdc85fb480e2b
Kamal Batcha Haja Mydeen <118987515+kamalbatcha1@users.noreply.github.com>
2026-06-04 12:24:28 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+RSS_FEEDS = {
+    "Tech": "https://feeds.bbci.co.uk/news/technology/rss.xml",
+    "Business": "https://feeds.bbci.co.uk/news/business/rss.xml"
+}
+
---
+++ ingest.py
@@ -0,0 +1,24 @@
+import feedparser
+from config import RSS_FEEDS
+
+def ingest_data():
+    articles = []
+
+    for source, url in RSS_FEEDS.items():
+        feed = feedparser.parse(url)
+
+        for entry in feed.entries[:5]:
+            articles.append({
+                "title": entry.title,
+                "link": entry.link
+            })
+
+    return articles
+
+
+
+
+
+
+
+
