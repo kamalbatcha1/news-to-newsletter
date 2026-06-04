78153b8f03c59c2dda45ee8bb140912a5655e3dd
Kamal Batcha Haja Mydeen <118987515+kamalbatcha1@users.noreply.github.com>
2026-06-04 12:28:09 +0530

    Add files via upload

---
+++ newsletter.py
@@ -0,0 +1,13 @@
+def generate_newsletter(articles):
+
+    print("\n --- NEWSLETTER FROM SSG EVENTS --- \n")
+
+    with open("newsletter.txt", "w", encoding="utf-8") as f:
+
+        for a in articles:
+
+            print(a["category"], "|", a["title"], "|", round(a["score"], 2))
+
+            f.write(f"[{a['category']}] {a['title']}\n")
+            f.write(f"{a['summary']}\n")
+            f.write(f"{a['link']}\n\n")
